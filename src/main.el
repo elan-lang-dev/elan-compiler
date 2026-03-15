89844e78f10144f01579223e5037e412e018c21a
elan-lang-dev <phdgandhimathi@gmail.com>
2026-03-15 13:52:53 +0530

    Update main.el

--- src/main.el
+++ src/main.el
@@ -1,5 +1,27 @@
-fn main {
+# ELAN Compiler Entry Point
+# Owner: Elancheran C G
 
-    print "ELAN compiler started"
+import lexer
+import parser
+import compiler
+import runtime
 
-}
+function main()
+
+    pri("ELAN Compiler Starting...")
+
+    var file = input("Enter .el file to run: ")
+
+    var source = runtime.read_file(file)
+
+    var tokens = lexer.tokenize(source)
+
+    var ast = parser.parse(tokens)
+
+    var program = compiler.compile(ast)
+
+    runtime.execute(program)
+
+end
+
+main()

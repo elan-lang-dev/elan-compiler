fe374442dc5b6ce1c31f6cdc425b670c1896f790
elan-lang-dev <phdgandhimathi@gmail.com>
2026-03-15 16:07:34 +0530

    Update elan.el

--- cli/elan.el
+++ cli/elan.el
@@ -1,91 +1,41 @@
-module elan
+# elan.el
+function main()
+    var args = system.args()
 
-# ELAN Command Line Interface
+    if arrlen(args) == 0
+        pri("ELAN CLI 1.0")
+        pri("Commands: run, repl, install, version, help")
+        return
+    end
 
-fn main args {
+    var cmd = args[0]
+    var file = args[1]
 
-    if size args == 0 {
+    if cmd == "run"
+        system.eval(system.readfile(file))
+    end
 
-        print "ELAN Command Line Tool"
-        print "Usage:"
-        print "elan run <file>"
-        print "elan compile <file>"
-        print "elan install <package>"
-        print "elan version"
+    if cmd == "repl"
+        while true
+            var input = system.input("> ")
+            if input == "exit()"
+                break
+            end
+            system.eval(input)
+        end
+    end
 
-    }
+    if cmd == "install"
+        system.run("elpm install " + file)
+    end
 
-    cmd = args[0]
+    if cmd == "version"
+        pri("ELAN 1.0")
+    end
 
-    if cmd == "run" {
+    if cmd == "help"
+        pri("Usage: elan <command> [file/package]")
+    end
+end
 
-        file = args[1]
-        run file
-
-    }
-
-    if cmd == "compile" {
-
-        file = args[1]
-        compile file
-
-    }
-
-    if cmd == "install" {
-
-        pkg = args[1]
-        install pkg
-
-    }
-
-    if cmd == "version" {
-
-        version
-
-    }
-
-}
-
-# run program
-fn run file {
-
-    print "Running program:"
-    print file
-
-}
-
-# compile program
-fn compile file {
-
-    print "Compiling program:"
-    print file
-
-}
-
-# install package
-fn install pkg {
-
-    print "Checking package registry..."
-
-    registry = "https://raw.githubusercontent.com/elan-lang-dev/elan-packages/main/index.json"
-
-    print "Downloading package:"
-    print pkg
-
-    url = "https://raw.githubusercontent.com/elan-lang-dev/elan-packages/main/packages/" + pkg + ".el"
-
-    print "Saving to packages folder"
-
-    print "Package installed successfully"
-
-}
-
-# version info
-fn version {
-
-    print "ELAN Programming Language"
-    print "Version 1.0"
-    print "Owner: Elancheran C G"
-    print "Co-Owner: Elandhiraiyan C G"
-
-}
+main()
